944f9a227470fee39a338f5b1e567119d1c534c0
Samuel fernandes <samuelfernandes10001@gamil.com>
2024-03-29 14:02:05 +0530

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import Image from "next/image";
+import img1 from "@/public/images/cooking.jpg";
+import img2 from "@/public/images/dictionary.jpg";
+import Link from "next/link";
+import img3 from "@/public/images/nutrition.jpg";
+import img4 from "@/public/images/tourism.jpeg";
+import img5 from "@/public/images/history.jpeg";
+import img6 from "@/public/images/weather.jpeg";
 
-import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Navigation, Pagination } from 'swiper/modules';
-import Image from 'next/image';
-import img1 from "@/public/images/img_1.jpg"
-import img2 from "@/public/images/img_2.jpg"
-// import img3 from "@/public/images/img_3.jpg"
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Home() {
   return (
@@ -20,18 +24,52 @@ export default function Home() {
           navigation
           pagination={{ type: "fraction" }}
           modules={[Navigation, Pagination]}
-          className="w-full"
+          className="w-[220px]"
         >
           <SwiperSlide>
-            {/* <Image src={img1} width={120} alt='mo' /> */}
-            slide 1
+            <div className="text-center">
+              <Link href={"/CookingPage"}>
+                <Image src={img1} width={220} alt="mo" />
+              </Link>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            {/* <Image src={img2} width={120} alt='mo' /> */}
-            slide 2
+            <div className="text-center">
+              <Link href={"/DictionaryPage"}>
+                <Image src={img2} width={220} alt="mo" />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="text-center">
+              <Link href={"/NutritionPage"}>
+                <Image src={img3} width={220} alt="mo" />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="text-center">
+              <Link href={"/TourismPage"}>
+                <Image src={img4} width={220} alt="mo" />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="text-center">
+              <Link href={"/HistoryPage"}>
+                <Image src={img5} width={220} alt="mo" />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="text-center">
+              <Link href={"/WeatherPage"}>
+                <Image src={img6} width={220} alt="mo" />
+              </Link>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
     </>
-  )
+  );
 }

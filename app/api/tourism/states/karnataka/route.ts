1a212942b9932eec2745fcbec5e4737266454b97
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-04-02 08:22:17 +0000

    karnataka

--- app/api/tourism/states/karnataka/route.ts
+++ app/api/tourism/states/karnataka/route.ts
@@ -2,10 +2,10 @@ import { NextResponse, NextRequest } from "next/server";
 
 
 const data = [
-    {places:[]},
+    {places:["Mysuru Palace"]},
     {
     img: [
-          
+          "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/40/63/77/the-palace-after-the.jpg?w=1100&h=-1&s=1"
         ]
     }
 ];
